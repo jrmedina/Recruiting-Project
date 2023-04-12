8715009baefc304323b89e2e2e52e2fc4d11c9b6
jmedina <ndgns2@gmail.com>
2023-04-12 13:20:44 -0600

    Refactor: Validation back to conditons

--- src/utils.js
+++ src/utils.js
@@ -1,15 +1,19 @@
 export function validateGuestData(guestData, remainingCapacity) {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  switch (true) {
-    case !emailRegex.test(guestData.email):
-      return "Please enter a valid email address.";
-    case guestData.tickets === 0 && remainingCapacity === 0:
-      return "There are no tickets left.";
-    case !guestData.tickets || guestData.tickets < 0:
-      return "Please enter a valid number of tickets.";
-    case remainingCapacity === 0:
-      return "You got the last ticket!";
-    default:
-      return "";
+
+  if (!emailRegex.test(guestData.email)) {
+    return "Please enter a valid email address.";
+  }
+
+  if (guestData.tickets === 0 && remainingCapacity === 0) {
+    return "There are no tickets left.";
+  }
+
+  if (!guestData.tickets || guestData.tickets < 0) {
+    return "Please enter a valid number of tickets.";
+  }
+
+  if (remainingCapacity === 0) {
+    return "You got the last ticket!";
   }
 }
