0b49c9f3a3bfa167b046f75e8163085f6f3e20c9
jmedina <ndgns2@gmail.com>
2023-04-12 12:39:35 -0600

    Prettier

--- src/guest-repository.js
+++ src/guest-repository.js
@@ -1,31 +1,30 @@
-
 const defaultGuests = [
-  { email: 'gangnam-style@psy.com', tickets: 1 },
-  { email: 'harlem.shake@baauer.com', tickets: 8 },
-  { email: 'black.beatles@skrillex.com', tickets: 3 },
+  { email: "gangnam-style@psy.com", tickets: 1 },
+  { email: "harlem.shake@baauer.com", tickets: 8 },
+  { email: "black.beatles@skrillex.com", tickets: 3 },
 ];
-const KEY = 'iscout-recruit-project.guests';
+const KEY = "iscout-recruit-project.guests";
 
 module.exports = class GuestRepository {
   load() {
-    return new Promise(resolve => {
+    return new Promise((resolve) => {
       setTimeout(() => {
         try {
           const json = localStorage.getItem(KEY);
-          if (!json) throw 'no data found';
+          if (!json) throw "no data found";
           resolve(JSON.parse(json));
-        }catch (e) {
+        } catch (e) {
           resolve(defaultGuests);
         }
       }, 1000);
     });
   }
   save(guests) {
-    return new Promise(resolve => {
+    return new Promise((resolve) => {
       setTimeout(() => {
         try {
           localStorage.setItem(KEY, JSON.stringify(guests));
-        }catch (e) {
+        } catch (e) {
           // noop
         }
         resolve();
@@ -33,11 +32,11 @@ module.exports = class GuestRepository {
     });
   }
   reset() {
-    return new Promise(resolve => {
+    return new Promise((resolve) => {
       setTimeout(() => {
         localStorage.removeItem(KEY);
         resolve();
       }, 1000);
     });
   }
-}
+};
